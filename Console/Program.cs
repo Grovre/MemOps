5814d444b8687343140e45d3edab7748031a35f2
Grovre <50428844+Grovre@users.noreply.github.com>
2023-01-31 19:01:29 -0600

    Read/Write and console tests

---
+++ Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using MemOps;
+
+namespace Console;
+
+public static unsafe class Program
+{
+    public static void Main()
+    {
+        var handle = Process.GetCurrentProcess().SafeHandle;
+        var n = 24;
+        MemoryOps.Read(handle, &n, out int read); // Reading from n to read
+        System.Console.WriteLine(read);
+        n = int.MaxValue;
+        MemoryOps.Write(handle, &read, ref n); // Writing from n to read
+        System.Console.WriteLine(read);
+    }
+}
---
+++ MemOps/MemoryOps.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace MemOps;
+
+public static unsafe class MemoryOps
+{
+    public static ref T Read<T>(SafeHandle handle, void* baseAddress, out T bufferStruct)
+        where T: struct
+    {
+        handle.IsMemoryValid();
+        bufferStruct = default;
+        var sz = (nuint)Marshal.SizeOf(bufferStruct);
+        nuint readBytes = default;
+        fixed (void* bufPtr = &bufferStruct)
+        {
+            var result = PInvoke.ReadProcessMemory(handle, baseAddress, bufPtr, sz, &readBytes);
+            Debug.Assert(result);
+        }
+        Console.WriteLine($"Read {readBytes} bytes at 0x{((nint)baseAddress):X}");
+        return ref bufferStruct;
+    }
+
+    public static void Write<T>(SafeHandle handle, void* baseAddress, ref T bufferStruct)
+        where T: struct
+    {
+        handle.IsMemoryValid();
+        var sz = (nuint)Marshal.SizeOf(bufferStruct);
+        nuint writtenBytes = default;
+        fixed (void* bufPtr = &bufferStruct)
+        {
+            var result = PInvoke.WriteProcessMemory(handle, baseAddress, bufPtr, sz, &writtenBytes);
+            Debug.Assert(result);
+        }
+        Console.WriteLine($"Wrote {writtenBytes} bytes at 0x{((nint)baseAddress):X}");
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static void IsMemoryValid(this SafeHandle handle)
+    {
+        Debug.Assert(handle != null);
+        Debug.Assert(!handle.IsInvalid);
+        Debug.Assert(!handle.IsClosed);
+    }
+}
