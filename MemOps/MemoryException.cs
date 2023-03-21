84d326688accdc51d8eb2fbc99a3b16d00c41193
Grovre <50428844+Grovre@users.noreply.github.com>
2023-03-21 00:49:21 -0500

    MemoryException type for error handling

---
+++ MemOps/MemoryException.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace MemOps;
+
+public sealed class MemoryException : ExternalException
+{
+    public static MemoryException FromMostRecentPInvokeError()
+        => new(Marshal.GetLastPInvokeErrorMessage(), Marshal.GetLastPInvokeError());
+
+    public MemoryException()
+    {
+    }
+
+    public MemoryException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public MemoryException(string? message) : base(message)
+    {
+    }
+
+    public MemoryException(string? message, Exception? inner) : base(message, inner)
+    {
+    }
+
+    public MemoryException(string? message, int errorCode) : base(message, errorCode)
+    {
+    }
+}
