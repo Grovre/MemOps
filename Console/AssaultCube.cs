716f9912e63f2a8700d02c1f60ce41c9ac395348
Grovre <50428844+Grovre@users.noreply.github.com>
2023-03-24 22:46:44 -0500

    assaultcube example

---
+++ Console/AssaultCube.cs
@@ -0,0 +1,15 @@
+﻿namespace Console;
+
+public class AssaultCube
+{
+    public const string ModuleName = "ac_client";
+    public const int MaxPlayers = 32;
+    public static nint[] MyPlayerOffsets = new nint[] { 0x0017B0B8, 0x0 };
+    public static nint[] EntityListOffsets = new nint[] { 0x187C10, 0x0 };
+    public static nint[] FromEntityAddressToNameOffsets = new nint[] { 0x205 };
+    public static nint[] FromEntityAddressToRifleAmmoOffsets = new nint[] { 0x140 };
+    public static nint[] FromEntityAddressToHeadXPosOffsets = new nint[] { 0x4 };
+    public static nint[] FromEntityAddressToHeadYPosOffsets = new nint[] { 0xC };
+    public static nint[] FromEntityAddressToHeadZPosOffsets = new nint[] { 0x8 };
+    public static nint[] FromEntityAddressToFeetZPosOffsets = new nint[] { 0x2C };
+}
--- Console/Program.cs
+++ Console/Program.cs
@@ -2,67 +2,51 @@
 
 using System.Diagnostics;
 using System.Globalization;
+using System.Numerics;
 using MemOps.DataStructures;
 using MemOps.Enums;
 using MemOps.Ops;
 
 namespace Console;
 
-public static class Program
+public static unsafe class Program
 {
-    private const int Pid = 18600;
-    private const nint BaseAddress = 0x4B4C9A00;
-    private static readonly nint[] OffsetsFromBaseAddress = { 0x60, 0x1F0, 0x88, 0x38, 0x1A8, 0x158, 0x330 };
-
     public static void Main()
     {
-        var p = Process.GetProcessesByName("jetbrains-toolbox")[0];
-        var m = p.SearchModuleByFileNameIgnoreCase("zip.dll");
-        if (m == null)
-        {
-            System.Console.WriteLine("No module by that name");
-            return;
-        } 
-        
-        System.Console.WriteLine($"Module file name: {m.FileName}");
-        System.Console.WriteLine($"Module mod name: {m.ModuleName}");
-        System.Console.WriteLine($"Base address: 0x{m.BaseAddress:X}");
-        System.Console.WriteLine($"Entry address: 0x{m.EntryPointAddress:X}");
-    }
-    
-    public static void Main_o()
-    {
-        var proc = Process.GetProcessById(Pid);
+        using var proc = Process.GetProcessesByName(AssaultCube.ModuleName)[0];
+        var baseAddr = proc.MainModule!.BaseAddress;
         using var handle = proc.OpenProcessSafeHandle(ProcessAccessRights.ProcessAllAccess);
-        var address = MemoryOps.FollowOffsets(handle, BaseAddress, OffsetsFromBaseAddress);
 
-        var memWorker = new BufferedMemoryAddress<int>(handle, address, false);
-        memWorker.Read();
-        System.Console.WriteLine(memWorker.Buffer);
-    }
+        var myPlayerAddr = MemoryOps.FollowOffsets(handle, baseAddr, AssaultCube.MyPlayerOffsets);
+        System.Console.WriteLine(myPlayerAddr.ToString("X"));
 
-    static void PrintProcessesAndNamesAlphabetical()
-    {
-        System.Console.WriteLine("Process name: ID");
-        System.Console.WriteLine(string.Join('\n', Process.GetProcesses()
-            .Select(p => $"{p.ProcessName}: {p.Id}")
-            .Order()));
-        System.Console.WriteLine();
-    }
-
-    static Process ReadProcess()
-    {
-        System.Console.WriteLine("Enter PID: ");
-        var n = int.Parse(System.Console.ReadLine()!);
-        return Process.GetProcessById(n);
-    }
-
-    static nint ReadAddress()
-    {
-        System.Console.WriteLine("Enter hex address: ");
-        return nint.Parse(
-            System.Console.ReadLine()!.TrimStart('0', 'x', 'X'),
-            NumberStyles.HexNumber,
-            null); 
+        for (var i = 0; i < 1_000; i++)
+        {
+            Thread.Sleep(10);
+            var rifleAmmo = new BufferedMemoryAddress<int>(
+                handle, 
+                MemoryOps.FollowOffsets(handle, myPlayerAddr, AssaultCube.FromEntityAddressToRifleAmmoOffsets),
+                false);
+            rifleAmmo.Buffer = 999;
+            rifleAmmo.Write();
+            
+            var x = new BufferedMemoryAddress<float>(
+                handle,
+                MemoryOps.FollowOffsets(handle, myPlayerAddr, AssaultCube.FromEntityAddressToHeadXPosOffsets),
+                false);
+            var y = new BufferedMemoryAddress<float>(
+                handle,
+                MemoryOps.FollowOffsets(handle, myPlayerAddr, AssaultCube.FromEntityAddressToHeadYPosOffsets),
+                false);
+            var z = new BufferedMemoryAddress<float>(
+                handle,
+                MemoryOps.FollowOffsets(handle, myPlayerAddr, AssaultCube.FromEntityAddressToFeetZPosOffsets),
+                false);
+            var v = new Vector3();
+            x.Read(out v.X);
+            y.Read(out v.Y);
+            z.Read(out v.Z);
+            System.Console.WriteLine(v);
+        }
     }
 }
