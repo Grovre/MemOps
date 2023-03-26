d433cd3c3aaedbc1276926cc648bde27e1807174
Grovre <50428844+Grovre@users.noreply.github.com>
2023-03-26 02:19:04 -0500

    IntPtr extensions to write less code

---
+++ MemOps/Extensions/IntPtrExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using MemOps.DataStructures;
+
+namespace MemOps.Extensions;
+
+public static class IntPtrExtensions
+{
+    public static BufferedMemoryAddress<T> ToAddress<T>(
+        this nint address, 
+        SafeHandle handle,
+        bool printOnReadOrWrite = false) 
+        where T : struct => new BufferedMemoryAddress<T>(handle, address, printOnReadOrWrite);
+}
